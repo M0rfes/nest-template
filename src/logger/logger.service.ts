80a18bba391531a67761a93b2bd5e71f4db7c157
M0rfes <fahimkhan20148@gmail.com>
2023-08-23 09:59:50 +0530

    added custom logger

---
+++ src/logger/logger.service.ts
@@ -0,0 +1,33 @@
+import {
+  ConsoleLogger,
+  Injectable,
+  LoggerService,
+} from '@nestjs/common';
+
+@Injectable()
+export class Logger extends ConsoleLogger implements LoggerService {}
+
+// @Injectable()
+// export class ExampleLogger implements LoggerService {
+//   log(message: any, ...optionalParams: any[]) {
+//     throw new Error('Method not implemented.');
+//   }
+//   error(message: any, ...optionalParams: any[]) {
+//     throw new Error('Method not implemented.');
+//   }
+//   warn(message: any, ...optionalParams: any[]) {
+//     throw new Error('Method not implemented.');
+//   }
+//   debug?(message: any, ...optionalParams: any[]) {
+//     throw new Error('Method not implemented.');
+//   }
+//   verbose?(message: any, ...optionalParams: any[]) {
+//     throw new Error('Method not implemented.');
+//   }
+//   fatal?(message: any, ...optionalParams: any[]) {
+//     throw new Error('Method not implemented.');
+//   }
+//   setLogLevels?(levels: LogLevel[]) {
+//     throw new Error('Method not implemented.');
+//   }
+// }
