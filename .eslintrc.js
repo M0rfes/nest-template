af5e3f08c6a10e1dfa77dd197c824db7c75ef245
Faheem Khan <faheemkhan@Faheems-MacBook-Air.local>
2023-08-31 16:09:47 +0530

    added new lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'check-file'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -21,5 +21,35 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'check-file/filename-blocklist': [
+      'error',
+      {
+        '**/*.model.ts': '*.models.ts',
+        '**/*.util.ts': '*.utils.ts'
+      }
+    ],
+    'check-file/folder-match-with-fex': [
+      'error',
+      {
+        '*.test.{js,ts}': '**/__tests__/',
+      }
+    ],
+    'check-file/folder-naming-convention': [
+      'error',
+      {
+        'src/**/': 'CAMEL_CASE',
+        'mocks/*/': 'KEBAB_CASE'
+      }
+    ],
+    '@typescript-eslint/naming-convention': ['error', { selector: 'variableLike', format: ['camelCase', 'UPPER_CASE'] }],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'variable',
+        types: ['boolean'],
+        format: ['PascalCase'],
+        prefix: ['is', 'should', 'has', 'can', 'did', 'will']
+      }
+    ]
   },
 };
