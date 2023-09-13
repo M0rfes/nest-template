d3c1981f9eaa6c8a6268c3b898fd1bf6421c9199
Faheem Khan <faheemkhan@Faheems-MacBook-Air.local>
2023-09-13 11:08:51 +0530

    added expesion handeling

--- src/core/interceptors/exception.interceptor.ts
+++ src/core/interceptors/exception.interceptor.ts
@@ -14,17 +14,18 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const context = host.switchToHttp();
     const response = context.getResponse<Response>();
     const i18n = I18nContext.current(host);
-    if (exception.getStatus() === 400) response.send(exception.getResponse());
+    if (exception.getStatus() === 400)
+      return response.send(exception.getResponse());
     const statusCode =
       exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
     const message = exception?.message
-      ? i18n.translate(`translation.${exception.message}`, {
+      ? i18n?.translate(`translation.${exception.message}`, {
           lang: i18n.lang,
         })
       : 'Internal server error';
-    response.status(statusCode).json({
+    return response.status(statusCode).json({
       statusCode,
       message,
     });
